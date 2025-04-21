0b74ed6a198eba84542dc8a8c2ce865c1e77152a
bendib-bouthaina <154970265+bendib-bouthaina@users.noreply.github.com>
2025-04-21 18:05:24 +0100

    Update 3_1_oldest.sql

--- 3_1_oldest.sql
+++ 3_1_oldest.sql
@@ -1,1 +1,13 @@
+-- Use the database
+USE LabDatabase;
+GO
+
+-- Display the name of the oldest Chercheur
+SELECT NomCh
+FROM Chercheur
+WHERE date_naissance = (
+    SELECT MIN(date_naissance)
+    FROM Chercheur
+);
+GO
 
