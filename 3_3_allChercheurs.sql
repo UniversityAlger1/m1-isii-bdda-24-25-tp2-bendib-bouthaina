58144ddeda6b7739c1f23c6673ce5f686cc715c5
bendib-bouthaina <154970265+bendib-bouthaina@users.noreply.github.com>
2025-04-21 18:06:25 +0100

    Update 3_3_allChercheurs.sql

--- 3_3_allChercheurs.sql
+++ 3_3_allChercheurs.sql
@@ -1,1 +1,10 @@
+-- Use the database
+USE LabDatabase;
+GO
+
+-- Display all Chercheur (NumCh, NomCh) from Laboratoire with CodeLab = '0001'
+SELECT NumCh, NomCh
+FROM Chercheur
+WHERE CodeLab = '0001';
+GO
 
