4e1dc4f89f3ae8ab840621a8ce180ca3041f219b
bendib-bouthaina <154970265+bendib-bouthaina@users.noreply.github.com>
2025-04-21 18:05:49 +0100

    Update 3_2_recent.sql

--- 3_2_recent.sql
+++ 3_2_recent.sql
@@ -1,1 +1,13 @@
+-- Use the database
+USE LabDatabase;
+GO
+
+-- Display the name of the most recently created Laboratoire
+SELECT NomLab
+FROM Laboratoire
+WHERE Datcreation = (
+    SELECT MAX(Datcreation)
+    FROM Laboratoire
+);
+GO
 
